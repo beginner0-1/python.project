28939e1ff131374057d3ceb3654fc6c1a6e9af6f
beginner0-1 <singhyuvraj7870@gmail.com>
2026-01-31 23:40:57 +0530

    improved

---
+++ calculator.py
@@ -0,0 +1,32 @@
+
+print("Welcome to Calculator")
+
+print("Choose operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == "1":
+    print("Result:", num1 + num2)
+
+elif choice == "2":
+    print("Result:", num1 - num2)
+
+elif choice == "3":
+    print("Result:", num1 * num2)
+
+elif choice == "4":
+    if num2 == 0:
+        print("Can't divide by zero")
+    else:
+        print("Result:", num1 / num2)
+
+else:
+    print("Invalid choice")
+    print (" re-enter, your choice ")
